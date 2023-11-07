2c442ba25b803227f4d0341036e3be928f2f8633
techakhil-me <techakhilc47@gmail.com>
2023-11-07 16:12:10 +0530

    responsive fix

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -6,11 +6,11 @@ import { Masonry } from "react-masonry";
 function page() {
   function show() {}
   return (
-    <section className="w-full pt-40 flex flex-col gap-20 items-start">
-      <div className="w-full sticky top-20 lg:text-left text-center text-white text-opacity-90 text-8xl lg:text-[200px] font-bold font-['Chakra Petch'] uppercase tracking-widest">
+    <section className="w-full relative pt-40 flex flex-col gap-20 items-start">
+      <div className="w-full sticky top-0 lg:text-left text-center text-white text-opacity-90 text-6xl md:text-8xl lg:text-[200px] font-bold font-['Chakra Petch'] uppercase tracking-widest">
         EVENTS
       </div>
-      <div className="w-full flex  items-center justify-center gap-10">
+      <div className="w-full flex md:flex-row flex-wrap flex-col items-center justify-center gap-10">
           <EventsCard title="Inauguration - 2023" img="/events/1.jpg" desc="lorem" /> 
           <EventsCard title="Inauguration - 2023" img="/events/1.jpg" desc="lorem" /> 
       </div>
--- app/layout.js
+++ app/layout.js
@@ -68,9 +68,9 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} >
         <Navbar />
-        <main className=" md:p-4  px-2 md:px-10 text-white text-opacity-90">
+        <main className=" md:p-4  overflow-hidden md:px-10 text-white text-opacity-90">
           {children}
         </main>
         <Footer />
